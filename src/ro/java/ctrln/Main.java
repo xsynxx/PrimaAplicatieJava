95d6b2eeb9147dc5da7435408cb1309cb2bb08c7
sy.nx <sy.nx@yahoo.com>
2022-05-03 13:53:37 +0300

    Initial commit

---
+++ src/ro/java/ctrln/Main.java
@@ -0,0 +1,9 @@
+package ro.java.ctrln;
+
+public class Main {
+    //JAR - Java Archive
+
+    public static void main(String[] args) {
+	    System.out.println("Invat limbajul Java si despre masina virtuala Java!");
+    }
+}
